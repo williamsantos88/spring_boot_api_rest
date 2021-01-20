c861d567716124b96ddbb196275931889f99ae03
williamsantos88 <46947008+williamsantos88@users.noreply.github.com>
2021-01-20 12:36:01 -0300

    Incluindo Cache no projeto
    O cache implementado não são para fins produtivos, pois usaremos um do próprio spring que utiliza maps. O ideal em Produção é um Redis ou um plataforma para gerenciamento do cache.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -43,6 +44,7 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
+	@Cacheable(value = "listaDeTopicos")
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @PageableDefault(sort = "id", direction = Direction.DESC, page = 0,  size = 0) Pageable paginacao) {
 		
 		if(nomeCurso == null) {
